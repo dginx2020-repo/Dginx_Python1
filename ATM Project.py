cd41a2e53d22b413b9711375dfb0361ce0719c1a
dginx2020-repo <dginx2020@gmail.com>
2020-08-03 00:40:42 +0530

    Billpay and balance add added

--- ATM Project.py
+++ ATM Project.py
@@ -34,6 +34,29 @@ while chances >=0:
                     else:
                         print('Pin did not matched Try later')
                         break
+            elif option == 3:
+                balanceadd = float(input('Enter the value you want to add: '))
+                balance = balance + balanceadd
+                print('your new balance is $', float(balance))
+                restart = input("Would you like to go back ?")
+                if restart in ('n', 'N', 'No', 'NO', 'no'):
+                    print('Thank you Mr. ', cname, 'Have a great Day')
+                    break
+            elif option == 4:
+                vendor = input("Enter vendor's name ")
+                billpay = float('Enter the Bill Amount ')
+                billconform = input('press Y to conform and N to reject the payment')
+                if billconform in ('Y', 'y',):
+                    balance = balance-billpay
+                    print('Transaction of $', float(billpay), 'is successful\n')
+                    print('Your available balance is $ ', balance)
+                    restart = input("Would you like to go back ?")
+                    if restart in ('n', 'N', 'No', 'NO', 'no'):
+                        print('Thank you Mr. ', cname, 'Have a great Day')
+                        break
+
+
+
 
 
 
