006427403e1a0315320038d427586211ded579ff
dginx2020-repo <dginx2020@gmail.com>
2020-08-03 01:01:43 +0530

    Withdraw added

--- ATM Project.py
+++ ATM Project.py
@@ -3,7 +3,7 @@ cname = input('Enter your Name ')
 print('Hello Mr', cname)
 
 # variable Declaration
-restart=('Y')
+restart=('y')
 chances = 3
 balance = 100000.86
 
@@ -19,12 +19,14 @@ while chances >=0:
             print('Press 4 for Billpay\n')
             print('Press 5 for Cash withdraw\n')
             option = int(input('Please Select your option '))
+# Balance enquiry
             if option == 1:
                 print('Mr', cname, 'your Balance is $', balance)
                 restart = input("Would you like to go back?")
                 if restart in ('n', 'N', 'No', 'NO', 'no'):
                     print('Thank you Mr. ', cname, 'Have a great Day')
                     break
+# PIN Change
             elif option == 2:
                     newpin = int(input('Please enter your new PIN '))
                     confpin = int(input('Please conform your new PIN '))
@@ -34,6 +36,7 @@ while chances >=0:
                     else:
                         print('Pin did not matched Try later')
                         break
+# Pay In
             elif option == 3:
                 balanceadd = float(input('Enter the value you want to add: '))
                 balance = balance + balanceadd
@@ -42,18 +45,43 @@ while chances >=0:
                 if restart in ('n', 'N', 'No', 'NO', 'no'):
                     print('Thank you Mr. ', cname, 'Have a great Day')
                     break
+# Bill Pay
             elif option == 4:
                 vendor = input("Enter vendor's name ")
-                billpay = float('Enter the Bill Amount ')
-                billconform = input('press Y to conform and N to reject the payment')
+                billpay = float(input('Enter the Bill Amount '))
+                billconform = input('press Y to conform and N to reject the payment ')
                 if billconform in ('Y', 'y',):
                     balance = balance-billpay
                     print('Transaction of $', float(billpay), 'is successful\n')
                     print('Your available balance is $ ', balance)
-                    restart = input("Would you like to go back ?")
+                    restart = input("Would you like to go back ? ")
                     if restart in ('n', 'N', 'No', 'NO', 'no'):
                         print('Thank you Mr. ', cname, 'Have a great Day')
                         break
+# withdraw
+            elif option == 5:
+                option5 = ('y')
+                withdraw = int(input("Enter the amount you want to withdraw: \n", '$10', '$20', '$50', '$100'))
+                if withdraw in (10, 20, 50, 100):
+                    balance = balance - withdraw
+                    print('\n Your new Balance is $', balance)
+                    restart = input("Would you like to go back ? ")
+                    if restart in ('n', 'N', 'No', 'NO', 'no'):
+                        print('Thank you Mr. ', cname, 'Have a great Day')
+                        break
+                elif withdraw not in (10, 20, 50, 100):
+                    print('Invalid Entry Try again')
+                    restart = ('y')
+                elif withdraw ==1:
+                    withdraw = int(input("Enter the amount you want to withdraw: \n", '$10', '$20', '$50', '$100'))
+
+
+
+
+
+
+
+
 
 
 
